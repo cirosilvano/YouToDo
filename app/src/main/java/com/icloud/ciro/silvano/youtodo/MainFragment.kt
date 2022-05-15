03af70cf1201ab5d061aa142146c2685b649dab0
Ciro Silvano <ciro.silvano@icloud.com>
2022-05-15 15:56:50 +0200

    mainfragment modificato

--- app/src/main/java/com/icloud/ciro/silvano/youtodo/MainFragment.kt
+++ app/src/main/java/com/icloud/ciro/silvano/youtodo/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -19,11 +20,14 @@ class MainFragment : Fragment() {
 
     }
 
+    lateinit var todoRecyclerView: RecyclerView
+
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
         // Inflate the layout for this fragment
         val view=inflater.inflate(R.layout.fragment_main, container, false)
 
        val addBtn=view.findViewById<ImageButton>(R.id.addButton)
+        todoRecyclerView = view.findViewById(R.id.itemsList)
 
         /*Evento click dell'addButton*/
         addBtn.setOnClickListener {

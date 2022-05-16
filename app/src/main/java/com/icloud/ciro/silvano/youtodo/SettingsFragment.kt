a6e7db596f4497e1ea630d08e16756035ea00317
Ciro Silvano <ciro.silvano@icloud.com>
2022-05-16 19:54:58 +0200

    aggiunto menu impostazioni

---
+++ app/src/main/java/com/icloud/ciro/silvano/youtodo/SettingsFragment.kt
@@ -0,0 +1,57 @@
+package com.icloud.ciro.silvano.youtodo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import com.google.android.material.bottomappbar.BottomAppBar
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SettingsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SettingsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view=inflater.inflate(R.layout.fragment_edit, container, false)
+        val btnBack=view.findViewById<Button>(R.id.backSettingsButton)
+
+        btnBack.setOnClickListener {
+            val action=EditFragmentDirections.actionEditFragmentToMainFragment()
+            view.findNavController().navigate(action)
+        }
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SettingsFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+
+}

d05b37396581323cf34d2471f75e5468e214b311
Carlotta Schiavo <carly2000schiavo@gmail.com>
2022-05-26 16:30:29 +0200

    Aggiunta gestione stato persistente per i temi

--- app/src/main/java/com/icloud/ciro/silvano/youtodo/MainActivity.kt
+++ app/src/main/java/com/icloud/ciro/silvano/youtodo/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.icloud.ciro.silvano.youtodo
 
+import android.content.SharedPreferences
+import android.media.audiofx.Equalizer
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.android.material.color.DynamicColors
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class MainActivity : AppCompatActivity() {
 
-    var numberOfItems: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /* Gestione dello stato persistente per il corretto caricamento del tema impostato*/
+
+        var sharedPreferences=getSharedPreferences("themePref", MODE_PRIVATE)
+        var themeType=sharedPreferences.getInt("THEME",0)
+        Log.d("","PREFERENCES: ${themeType}")
+        when(themeType){
+            0->setTheme(R.style.Theme_YouToDo)
+            1-> setTheme(R.style.Theme_YouToDoWinter)
+            2->  setTheme(R.style.Theme_YouToDoFall)
+        }
         setContentView(R.layout.activity_main)
 
 
     }
+
+
 
 }
--- app/src/main/java/com/icloud/ciro/silvano/youtodo/SettingsFragment.kt
+++ app/src/main/java/com/icloud/ciro/silvano/youtodo/SettingsFragment.kt
@@ -1,6 +1,8 @@
 package com.icloud.ciro.silvano.youtodo
 
+import android.content.Context.MODE_PRIVATE
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ShareActionProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.transition.TransitionInflater
@@ -20,7 +23,9 @@ class SettingsFragment : Fragment() {
 
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
-
+    private val my_themes=listOf("Theme_YouToDoWinter","Theme_YouToDoFall")
+    private  var  sharedPreferences:SharedPreferences?=null
+    var prefEditor:SharedPreferences.Editor?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val inflater = TransitionInflater.from(requireContext())
@@ -32,25 +37,34 @@ class SettingsFragment : Fragment() {
 
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
 
+        sharedPreferences=activity?.getSharedPreferences("themePref",MODE_PRIVATE)
+        prefEditor=sharedPreferences?.edit()
+
         binding.backSettingsButton.setOnClickListener {
             findNavController().navigate(R.id.action_settingsFragment_to_mainFragment)
+
         }
 
         binding.btnTheme1.setOnClickListener{
             activity?.setTheme(R.style.Theme_YouToDoWinter)
+            prefEditor?.putInt("THEME",1)
+            prefEditor?.commit()
             findNavController().navigate(R.id.action_settingsFragment_to_mainFragment)
 
         }
 
         binding.btnTheme2.setOnClickListener{
             activity?.setTheme(R.style.Theme_YouToDoFall)
+            prefEditor?.putInt("THEME",2)
+            prefEditor?.commit()
             findNavController().navigate(R.id.action_settingsFragment_to_mainFragment)
 
         }
 
 
         binding.btnReset.setOnClickListener{
-           // activity?.setTheme(R.style.Theme_YouToDo)
+            prefEditor?.putInt("THEME",0)
+            prefEditor?.commit()
             activity?.recreate()
             findNavController().navigate(R.id.action_settingsFragment_to_mainFragment)
 

27c39338cadb3e98806d84e523981bbb2dde7875
Ciro Silvano <ciro.silvano@icloud.com>
2022-05-28 22:19:16 +0200

    reimpostato date < oggi, da sistemare yesterday

--- app/src/main/java/com/icloud/ciro/silvano/youtodo/ItemAdapter.kt
+++ app/src/main/java/com/icloud/ciro/silvano/youtodo/ItemAdapter.kt
@@ -30,19 +30,20 @@ import java.time.Duration
 import java.time.LocalDate
 import java.time.Period
 
-class ItemAdapter(val onItemSwipeListener: OnItemSwipeListener) : RecyclerView.Adapter<ItemAdapter.MyViewHolder>() {
+class ItemAdapter(val onItemSwipeListener: OnItemSwipeListener) :
+    RecyclerView.Adapter<ItemAdapter.MyViewHolder>() {
 
-    private var itemList= emptyList<Item>()
+    private var itemList = emptyList<Item>()
 
-    class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val name: TextView = itemView.findViewById(R.id.itemName)
         val category: TextView = itemView.findViewById(R.id.itemCategory)
         val deadline: TextView = itemView.findViewById(R.id.itemDeadline)
-        val cardLayout : ConstraintLayout = itemView.findViewById(R.id.itemConstraintLayout)
-        val done : CheckBox = itemView.findViewById(R.id.cbDone)
-        val editCard : ImageButton = itemView.findViewById(R.id.editCard)
+        val cardLayout: ConstraintLayout = itemView.findViewById(R.id.itemConstraintLayout)
+        val done: CheckBox = itemView.findViewById(R.id.cbDone)
+        val editCard: ImageButton = itemView.findViewById(R.id.editCard)
 
-        fun bind(name_tx : String, category_tx : String, deadline_tx : String, checked : Boolean) {
+        fun bind(name_tx: String, category_tx: String, deadline_tx: String, checked: Boolean) {
             name.text = name_tx
             category.text = category_tx
             // deadline.text = deadline_tx
@@ -51,28 +52,55 @@ class ItemAdapter(val onItemSwipeListener: OnItemSwipeListener) : RecyclerView.A
             /* deadline styling */
             var deadline_gen = ""
             Log.d("", "deadline_tx for ${name_tx}: ${deadline_tx.length} -> $deadline_tx")
-            if(deadline_tx.length == 19) {
+            if (deadline_tx.length == 19) {
                 // date-time formats are 19 digits long
                 val ldt = LocalDateTime.parse(deadline_tx)
                 val ld = ldt.toLocalDate()
                 val ldToday = LocalDate.now()
                 val period = Period.between(ldToday, ld)
 
-                if(period.years == 0 && period.months == 0) {
+                if (period.years == 0 && period.months == 0 && ld.isAfter(ldToday)) {
                     val res = itemView.context.resources
                     when (period.days) {
-                        0 -> deadline_gen = "${res.getString(R.string.today)}, ${generateTime(ldt.hour, ldt.minute)}"
-                        1 -> deadline_gen = "${res.getString(R.string.tomorrow)}, ${generateTime(ldt.hour, ldt.minute)}"
+                        0 -> deadline_gen = "${res.getString(R.string.today)}, ${
+                            generateTime(
+                                ldt.hour,
+                                ldt.minute
+                            )
+                        }"
+                        1 -> deadline_gen = "${res.getString(R.string.tomorrow)}, ${
+                            generateTime(
+                                ldt.hour,
+                                ldt.minute
+                            )
+                        }"
                         else -> {
-                            if(period.days < 7) {
-                                deadline_gen = "${DateTimeFormatHelper.weekDays(ld.dayOfWeek, res)}, ${generateTime(ldt.hour, ldt.minute)}"
+                            if (period.days < 7) {
+                                deadline_gen = "${
+                                    DateTimeFormatHelper.weekDays(
+                                        ld.dayOfWeek,
+                                        res
+                                    )
+                                }, ${generateTime(ldt.hour, ldt.minute)}"
                             } else {
-                                deadline_gen = generateDateTime(ldt.year, ldt.monthValue, ldt.dayOfMonth, ldt.hour, ldt.minute)
+                                deadline_gen = generateDateTime(
+                                    ldt.year,
+                                    ldt.monthValue,
+                                    ldt.dayOfMonth,
+                                    ldt.hour,
+                                    ldt.minute
+                                )
                             }
                         }
                     }
                 } else {
-                    deadline_gen = generateDateTime(ldt.year, ldt.monthValue, ldt.dayOfMonth, ldt.hour, ldt.minute)
+                    if (ldToday.isAfter(ld)) {
+                        deadline_gen = if (period.days == 1) {
+                            "${R.string.yesterday}, ${generateTime(ldt.hour, ldt.minute)}"
+                        } else {
+                            generateDateTime(ldt.year, ldt.monthValue, ldt.dayOfMonth, ldt.hour, ldt.minute)
+                        }
+                    }
                 }
             }
             deadline.text = deadline_gen
@@ -101,13 +129,20 @@ class ItemAdapter(val onItemSwipeListener: OnItemSwipeListener) : RecyclerView.A
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.cardview_item, parent, false))
+        return MyViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.cardview_item, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         var currentItem = itemList[position]
 
-        holder.bind(currentItem.name.toString(), currentItem.category.toString(), currentItem.deadline.toString(), currentItem.isDone)
+        holder.bind(
+            currentItem.name.toString(),
+            currentItem.category.toString(),
+            currentItem.deadline.toString(),
+            currentItem.isDone
+        )
 
 
         holder.editCard.setOnClickListener {
@@ -131,46 +166,46 @@ class ItemAdapter(val onItemSwipeListener: OnItemSwipeListener) : RecyclerView.A
         })*/
 
         holder.cardLayout.setOnTouchListener(View.OnTouchListener { view, event ->
-                // variables to store current configuration of quote card.
-                val displayMetrics = holder.cardLayout.context.resources.displayMetrics
-                val cardWidth = holder.cardLayout.width
-                val cardStart = (displayMetrics.widthPixels.toFloat() / 2) - (cardWidth / 2)
+            // variables to store current configuration of quote card.
+            val displayMetrics = holder.cardLayout.context.resources.displayMetrics
+            val cardWidth = holder.cardLayout.width
+            val cardStart = (displayMetrics.widthPixels.toFloat() / 2) - (cardWidth / 2)
 
-                when (event.action) {
-                    MotionEvent.ACTION_MOVE -> {
-                        val newX : Float = event.rawX
-                        if (newX - cardWidth < cardStart) { // or newX < cardStart + cardWidth
-                            holder.cardLayout.animate().x(
-                                min(cardStart, newX - (cardWidth / 2))
-                            )
-                                .setDuration(0)
-                                .start()
-                        }
-                        true
+            when (event.action) {
+                MotionEvent.ACTION_MOVE -> {
+                    val newX: Float = event.rawX
+                    if (newX - cardWidth < cardStart) { // or newX < cardStart + cardWidth
+                        holder.cardLayout.animate().x(
+                            min(cardStart, newX - (cardWidth / 2))
+                        )
+                            .setDuration(0)
+                            .start()
                     }
-                    MotionEvent.ACTION_UP -> {
-                        var currentX = holder.cardLayout.x
-                        holder.cardLayout.animate()
-                            .x(cardStart)
-                            .setDuration(150)
-                            .setListener(object : AnimatorListenerAdapter() {
-                                override fun onAnimationEnd(animation: Animator) {
-                                    // check if the swipe distance was more than
-                                    // minimum swipe required to load a new quote
-                                    if (currentX < -80) {
-                                        // Add logic to load a new quote if swiped adequately
-                                        onItemSwipeListener.onItemSwipe(currentItem)
-                                        currentX = 0f
-                                    }
-
-                                }
-                            })
-                        true
-                    }
+                    true
                 }
-                true
+                MotionEvent.ACTION_UP -> {
+                    var currentX = holder.cardLayout.x
+                    holder.cardLayout.animate()
+                        .x(cardStart)
+                        .setDuration(150)
+                        .setListener(object : AnimatorListenerAdapter() {
+                            override fun onAnimationEnd(animation: Animator) {
+                                // check if the swipe distance was more than
+                                // minimum swipe required to load a new quote
+                                if (currentX < -80) {
+                                    // Add logic to load a new quote if swiped adequately
+                                    onItemSwipeListener.onItemSwipe(currentItem)
+                                    currentX = 0f
+                                }
 
+                            }
+                        })
+                    true
+                }
             }
+            true
+
+        }
         )
 
         holder.done.setOnClickListener {

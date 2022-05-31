9c62f02ea7ee1375b40671df15b0bd6554b86b32
Carlotta Schiavo <carly2000schiavo@gmail.com>
2022-05-31 15:58:07 +0200

    Rivoluzionata SettingsFragment.kt

--- app/src/main/java/com/icloud/ciro/silvano/youtodo/MainActivity.kt
+++ app/src/main/java/com/icloud/ciro/silvano/youtodo/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.icloud.ciro.silvano.youtodo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
@@ -12,13 +14,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var sharedPreferences=getSharedPreferences("themePref", MODE_PRIVATE)
-        var value=sharedPreferences.getInt("THEME",0)
-        when(value){
+        var sharedPrefTheme=getSharedPreferences("themePref", MODE_PRIVATE)
+
+        var valueTheme=sharedPrefTheme.getInt("THEME",0)
+        when(valueTheme){
             0-> setTheme(R.style.Theme_YouToDo)
             1->setTheme(R.style.Theme_YouToDoWinter)
             2->setTheme(R.style.Theme_YouToDoFall)
         }
+
+        var isDarkModeOn = sharedPrefTheme.getBoolean("MODE",false)
+
+        Log.d("","isDarkModeOn ${isDarkModeOn}")
+        if(isDarkModeOn){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+        else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+
     }
 
 }
--- app/src/main/java/com/icloud/ciro/silvano/youtodo/SettingsFragment.kt
+++ app/src/main/java/com/icloud/ciro/silvano/youtodo/SettingsFragment.kt
@@ -1,31 +1,26 @@
 package com.icloud.ciro.silvano.youtodo
 
 import android.content.Context.MODE_PRIVATE
-import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ShareActionProvider
-import androidx.navigation.findNavController
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.findNavController
 import androidx.transition.TransitionInflater
-import com.google.android.material.bottomappbar.BottomAppBar
-import com.google.android.material.color.DynamicColors
-import com.icloud.ciro.silvano.youtodo.databinding.FragmentMainBinding
 import com.icloud.ciro.silvano.youtodo.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
 
     private var _binding: FragmentSettingsBinding? = null
-    private val binding get() = _binding!!
-    private val my_themes=listOf("Theme_YouToDoWinter","Theme_YouToDoFall")
-    private  var  sharedPreferences:SharedPreferences?=null
-    var prefEditor:SharedPreferences.Editor?=null
+    private val binding get()=_binding!!
+    private var sharedPreferences:SharedPreferences?=null
+    private var prefEditor:SharedPreferences.Editor?=null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val inflater = TransitionInflater.from(requireContext())
@@ -37,42 +32,113 @@ class SettingsFragment : Fragment() {
 
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
 
-        sharedPreferences=activity?.getSharedPreferences("themePref",MODE_PRIVATE)
-        prefEditor=sharedPreferences?.edit()
+        //Inizializzazione dell'oggetto di classe SharePreferences. Tale oggetto servirà a prendersi
+        // i valori riguardabti gli stati dei ToggleButtons e dei temi salvati nel precendete uso dell'applicazione
+        sharedPreferences = activity?.getSharedPreferences("themePref", MODE_PRIVATE)
+        prefEditor = sharedPreferences?.edit()
 
+        //Stati dei ToggleButtons
+        var btnDarkState = sharedPreferences!!.getBoolean("TOGGLE_DARK", false)
+        var btnDefaultState = sharedPreferences!!.getBoolean("TOGGLE_DEFAULT", false)
+        var btnTheme1State = sharedPreferences!!.getBoolean("TOGGLE_THEME1", false)
+        var btnTheme2State = sharedPreferences!!.getBoolean("TOGGLE_THEME2", false)
+
+
+        //Impostazione dello stato dei toggleButtons appartenenti al gruppo dei temi
+        if (btnDefaultState) {
+            binding.toggleButtonTheme.check(R.id.btnDefault)
+        }
+        if (btnTheme1State) {
+            binding.toggleButtonTheme.check(R.id.btnTheme1)
+        }
+        if (btnTheme2State) {
+            binding.toggleButtonTheme.check(R.id.btnTheme2)
+        }
+
+        //Impostazione dello stato dei toggleButtons appartenenti al gruppo delle modalità
+        if (btnDarkState) {
+            binding.toggleButtonMode.check(R.id.btnDarkMode)
+        } else {
+            binding.toggleButtonMode.check(R.id.btnLightMode)
+        }
+
+        //Gestione della selezione dei toggleButtons appartenenti al gruppo dei temi. Per ogni
+        //tipo di selezione viene salvato un certo stato per i TogglesButtons del gruppo e per il tema.
+        binding.toggleButtonTheme.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
+
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.btnTheme1 -> {
+                        activity?.setTheme(R.style.Theme_YouToDoWinter)
+                        prefEditor?.putInt("THEME", 1)
+                        prefEditor?.putBoolean("TOGGLE_THEME1", isChecked)
+                        prefEditor?.putBoolean("TOGGLE_THEME2", !isChecked)
+                        prefEditor?.putBoolean("TOGGLE_DEFAULT", !isChecked)
+                        prefEditor?.commit()
+                        activity?.recreate()
+
+                    }
+                    R.id.btnTheme2 -> {
+
+                        activity?.setTheme(R.style.Theme_YouToDoFall)
+                        prefEditor?.putInt("THEME", 2)
+                        prefEditor?.putBoolean("TOGGLE_THEME1", !isChecked)
+                        prefEditor?.putBoolean("TOGGLE_THEME2", isChecked)
+                        prefEditor?.putBoolean("TOGGLE_DEFAULT", !isChecked)
+                        prefEditor?.commit()
+                        activity?.recreate()
+                    }
+
+                    R.id.btnDefault -> {
+                        prefEditor?.putInt("THEME", 0)
+                        prefEditor?.putBoolean("TOGGLE_THEME1", !isChecked)
+                        prefEditor?.putBoolean("TOGGLE_THEME2", !isChecked)
+                        prefEditor?.putBoolean("TOGGLE_DEFAULT", isChecked)
+                        prefEditor?.commit()
+                        activity?.recreate()
+                    }
+                }
+            }
+        }
+
+
+
+        //Gestione della selezione dei toggleButtons appartenenti al gruppo delle modalità. Per ogni
+        //tipo di selezione viene salvato un certo stato per i TogglesButtons del gruppo e per la modalità.
+        binding.toggleButtonMode.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
+
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.btnDarkMode -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                        prefEditor?.putBoolean("MODE", isChecked)
+                        prefEditor?.putBoolean("TOGGLE_DARK", isChecked)
+                        prefEditor?.putBoolean("TOGGLE_LIGHT", !isChecked)
+                        prefEditor?.apply()
+                    }
+                    R.id.btnLightMode -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        prefEditor?.putBoolean("MODE", !isChecked)
+                        prefEditor?.putBoolean("TOGGLE_DARK", !isChecked)
+                        prefEditor?.putBoolean("TOGGLE_LIGHT", isChecked)
+                        prefEditor?.apply()
+                    }
+                }
+            }
+        }
+
+        //Gestione tocco bottone che serve a tornare al mainFragment e che inoltre invia un messaggio di feedback all'utente
+        binding.btnApply.setOnClickListener {
+            var mex=getString(R.string.changesAppliedMex)
+            Toast.makeText(requireContext(), "${mex}", Toast.LENGTH_LONG).show()
+            findNavController().navigate(R.id.action_settingsFragment_to_mainFragment)
+        }
+
+        //Gestione tocco bottone che serve a tornare al mainFragment
         binding.backSettingsButton.setOnClickListener {
             findNavController().navigate(R.id.action_settingsFragment_to_mainFragment)
-
         }
 
-        binding.btnTheme1.setOnClickListener{
-            activity?.setTheme(R.style.Theme_YouToDoWinter)
-            prefEditor?.putInt("THEME",1)
-            prefEditor?.commit()
-            findNavController().navigate(R.id.action_settingsFragment_to_mainFragment)
-
-        }
-
-        binding.btnTheme2.setOnClickListener{
-            activity?.setTheme(R.style.Theme_YouToDoFall)
-            prefEditor?.putInt("THEME",2)
-            prefEditor?.commit()
-            findNavController().navigate(R.id.action_settingsFragment_to_mainFragment)
-
-        }
-
-
-        binding.btnReset.setOnClickListener{
-            prefEditor?.putInt("THEME",0)
-            prefEditor?.commit()
-            activity?.recreate()
-            findNavController().navigate(R.id.action_settingsFragment_to_mainFragment)
-
-        }
-
-        // Inflate the layout for this fragment
         return binding.root
     }
-
-
 }
